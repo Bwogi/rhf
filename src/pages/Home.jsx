860652a102b95047924bbb25437ef8f88d8a239e
Andrew Bwogi <andrew.bwogi@gmail.com>
2023-11-12 17:55:57 -0800

    date search disabled

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import Header from '../partials/Header';
-import Hero from '../partials/Hero';
-import Inspiration from '../partials/Inspiration';
-import Carousel from '../partials/Carousel';
-import Creatives from '../partials/Creatives';
+import Header from "../partials/Header";
+import Hero from "../partials/Hero";
+import Inspiration from "../partials/Inspiration";
+import Carousel from "../partials/Carousel";
+import Creatives from "../partials/Creatives";
 // import Pricing from '../partials/Pricing';
 // import Testimonials from '../partials/Testimonials';
-import Faqs from '../partials/Faqs';
-import Blog from '../partials/Blog';
-import Cta from '../partials/Cta';
-import Footer from '../partials/Footer';
-import NewHere from './NewHere';
-import About from './About';
-import Contact from './Contact';
-import Services from './Services';
-import SearchByDate from './SearchByDate';
+import Faqs from "../partials/Faqs";
+import Blog from "../partials/Blog";
+import Cta from "../partials/Cta";
+import Footer from "../partials/Footer";
+import NewHere from "./NewHere";
+import About from "./About";
+import Contact from "./Contact";
+import Services from "./Services";
+import SearchByDate from "./SearchByDate";
 
 function Home() {
   return (
@@ -27,20 +27,31 @@ function Home() {
       <main className="grow">
         {/*  Page sections */}
         <Hero />
-        <SearchByDate />
-        <div id='about'> <About /> </div>
-        <div id="services"><Services /></div>
-        <div id='newhere'><NewHere /></div>
+        {/* <SearchByDate /> */}
+        <div id="about">
+          {" "}
+          <About />{" "}
+        </div>
+        <div id="services">
+          <Services />
+        </div>
+        <div id="newhere">
+          <NewHere />
+        </div>
         {/* <div id='ministries'><Inspiration /></div> */}
-        
+
         {/* <Carousel /> */}
         {/* <Creatives /> */}
         {/* <Pricing /> */}
         {/* <Testimonials /> */}
         {/* <Faqs /> */}
-        <div id='blog'><Blog /></div> 
+        <div id="blog">
+          <Blog />
+        </div>
         <Cta />
-        <div id='contact'><Contact /></div>
+        <div id="contact">
+          <Contact />
+        </div>
       </main>
 
       {/*  Site footer */}
