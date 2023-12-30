b371eb94cb1ff54a2e3c610cb21d79f39ba1def2
Andrew Bwogi <andrew.bwogi@gmail.com>
2023-12-30 15:01:32 -0800

    changes to give button

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -67,7 +67,7 @@ function Header() {
               </li>
               <li className="ml-3">
                 <Link
-                  className="btn-sm text-white bg-[#1d5977] hover:bg-[#fea602] hover:text-white w-full shadow-sm"
+                  className="btn-sm text-white bg-[#fea602] hover:bg-[#1d5977] hover:text-white w-full shadow-sm"
                   to="/"
                 >
                   Give
